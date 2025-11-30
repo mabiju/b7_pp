c094c134619fc0d4da84f7f81b78743c6306ee0a
Vijaya Malla <mallabiju@gmail.com>
2025-11-30 14:26:04 +0545

    New Updates

---
+++ b_control_statments/i_if/b_nested_if.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+if(num < 100):
+    if(num < 50):
+        print("The number ", num, " is less than 50.")
+    if(num > 50):
+        print("The number ", num, " is greater than 50.")
+    if(num==50):
+        print("The number is ", num)
---
+++ b_control_statments/i_if/c_if_else.py
@@ -0,0 +1,6 @@
+# python program to check +ve or -ve using if else
+num = int(input("Enter a number to check +ve or -ve:"))
+if(num > 0):
+    print("The number is +ve")
+else:
+    print("The number is -ve")
